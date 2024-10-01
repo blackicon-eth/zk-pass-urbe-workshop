176c187aa1363934222f689b35c5388f593a35c4
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-10-01 13:35:10 +0200

    Minor change on comment

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
-//Sepolia Attestation contract address
-//You can add Sepolia chain from https://chainlist.org/?search=11155111&testnets=true
+// Sepolia Attestation contract address
+// You can add Sepolia chain from https://chainlist.org/?search=11155111&testnets=true
 export const contractAddress = "0x8c18c0436A8d6ea44C87Bf5853F8D11B55CF0302";
 
 // The zkPass allocator address
